f0437fd14c20f5a49e2add1d05620be886b7f4a8
my-first-project <dipakzope75@gmail.com>
2026-07-19 09:24:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('header nav');
 
+
 menuToggle.addEventListener('click', () => {
   const isOpen = nav.classList.toggle('open');
   menuToggle.classList.toggle('open', isOpen);
